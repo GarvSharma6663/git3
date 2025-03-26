dd91e9273a446acce3962ce22b5198ea9249b733
Garv Sharma <garvsharma663@gmail.com>
2025-03-26 15:13:55 +0530

    folder added

---
+++ New folder/add.java
@@ -0,0 +1,6 @@
+public class add {
+    public static void main(String[] args) {
+        int a = 3; int b = 5;
+        System.out.println("Addition is : " + (a+b));
+    }
+}
---
+++ New folder/subtract.java
@@ -0,0 +1,6 @@
+public class subtract {
+    public static void main(String[] args) {
+        int a = 5; int b = 3;
+        System.out.println("Addition is : " + (a-b));
+    }
+}
